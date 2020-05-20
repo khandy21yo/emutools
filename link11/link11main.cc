db6eadbf7a062c31b3cac151fcade89da097fd49
klap3 <kandy21yo@gmail.com>
2020-05-19 21:21:59 -0600

    link11: Add putconch yo link

--- link11/link11main.cc
+++ link11/link11main.cc
@@ -1,13 +1,27 @@
 //!\file link11main.cc
 //!
-//\brief Main driver code for link11
+//!\brief Main driver code for link11
 //!
+//!\mainpage
 //! main driver code for link11.
 //! Handles user command line processing,
 //! and main driver for the lionking process.
 //!
 //! Passes:
 //! 1. Load object file data.
+//! 2. Scan for all program sections and global variables.
+//! 3. Set absolute addresses for all program sections.
+//! 4. Handle relocations.
+//! 5. Write our binary code.
+//!
+//! 
+//! Development states.
+//! 1. Get it to read data from object files correctly.
+//! initilly hard coded to the files "hello.obj" and "putconch.obj".
+//! 2. Dump the loaded data to bbe sure it is reading correctly.
+//! 3. Handle the indivifual passes.
+//! 4. Geerate a functional binary file.
+//! 5. Create a command line parser to allow  for other code to be linked.
 //!
 //!\author Kevin Handy, Apr 2020
 //
@@ -25,7 +39,14 @@ int main(int argc, char **argv)
 	of.ReadFile(filename);
 	of.Dump(1);
 
+	ObjectFile of2;
+	std::string filename2 = "test/putconch.obj";
+
+	of2.ReadFile(filename2);
+	of2.Dump(1);
+
 	Link passes;
 	passes.PassTxt(of);
+	passes.PassTxt(of2);
 }
 
